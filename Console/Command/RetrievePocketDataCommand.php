2f8cf1233124816bf421212ea33489d9a7f9e257
Thierry Marianne <thierry.marianne@weaving-the-web.org>
2014-10-15 00:48:42 +0200

    retrieved and saved latest thousand items

--- Console/Command/RetrievePocketDataCommand.php
+++ Console/Command/RetrievePocketDataCommand.php
@@ -4,24 +4,27 @@ namespace WeavingTheWeb\OwnYourData\Console\Command;
 
 use GuzzleHttp\Client;
 
+use GuzzleHttp\Exception\ServerException;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
+use Symfony\Component\Console\Question\Question;
 use Symfony\Component\Translation\Loader\YamlFileLoader;
 use Symfony\Component\Translation\MessageSelector;
 use Symfony\Component\Translation\Translator;
 
 use Symfony\Component\Yaml\Parser;
 
-use WeavingTheWeb\OwnYourData\Exception\InstallationException;
+use WeavingTheWeb\OwnYourData\Exception\ConfigurationException;
 
 /**
  * @author  Thierry Marianne <thierry.marianne@weaving-the-web.org>
  */
 class RetrievePocketDataCommand extends Command
 {
-    const ITEMS_RETRIEVAL_LIMIT = 2;
+    const DEFAULT_ITEMS_RETRIEVAL_LIMIT = 10000;
 
     /**
      * @var \Symfony\Component\Translation\Translator
@@ -29,22 +32,52 @@ class RetrievePocketDataCommand extends Command
     private $translator;
 
     /**
+     * @var InputInterface
+     */
+    protected $input;
+
+    /**
      * @var OutputInterface
      */
     protected $output;
 
     /**
+     * @var string
+     */
+    protected $configurationFilePath;
+
+    /**
+     * Holds configuration for
+     *  - third-party Pocket application OAuth access token, consumer key
+     *  - Target directory (where retrieved items are to be stored)
+     *  - Application secret (used to generate OAuth state)
+     *
+     * @var array
+     */
+    protected $configuration;
+
+    /**
+     * @var string
+     */
+    public $configurationFilename = 'config.yml';
+
+    /**
      * @var \GuzzleHttp\Client
      */
-    public $client;
+    public $httpClient;
 
-    public $configurationFilename = 'config.yml';
+    /**
+     * Directory where items will be saved after retrieval
+     *
+     * @var string
+     */
+    public $targetDirectory;
 
     public function __construct($name = null)
     {
         parent::__construct($name);
 
-        $this->client = new Client();
+        $this->httpClient = new Client();
 
         $this->setupTranslator();
     }
@@ -55,7 +88,8 @@ class RetrievePocketDataCommand extends Command
     protected function configure()
     {
         $this->setName('weaving_the_web:pocket:retrieve')
-            ->setDescription("Command-line application to retrieve a user's pocket data via Pocket API");
+            ->setDescription("Command-line application to retrieve a user's pocket data via Pocket API")
+            ->setAliases(['wtw:pkt:get']);
     }
 
     /**
@@ -67,22 +101,42 @@ class RetrievePocketDataCommand extends Command
      */
     public function execute(InputInterface $input, OutputInterface $output)
     {
+        $this->input = $input;
         $this->output = $output;
 
-        $configurationFile = __DIR__ . '/../../Resources/config/' . $this->configurationFilename;
-        if ($this->canNotFindConfigurationFile($configurationFile)) {
-            InstallationException::throwNotFoundConfigurationException($configurationFile);
+        try {
+            $this->configurationFilePath = __DIR__ . '/../../Resources/config/' . $this->configurationFilename;
+
+            $this->validateConfigurationReadability();
+
+            $this->configuration = $this->parseConfiguration($this->configurationFilePath);
+
+            $this->targetDirectory = $this->configuration['items_retrieval']['target_directory'];
+            $this->validateTargetDirectory();
+
+            $this->validateConsumerKey();
+        } catch (ConfigurationException $exception) {
+            $output->writeln('<error>' . $exception->getMessage() . '</error>');
+
+            return $exception->getCode();
         }
 
-        $configuration = $this->parseConfiguration($configurationFile);
-        if ($this->isConsumerKeyMissing($configuration)) {
-            InstallationException::throwMissingConsumerKey($configurationFile);
-        }
+        $itemsRetrievalLimit = $this->getItemsRetrievalLimit();
 
-        $decodedJsonResponseBody = $this->retrieveItems($configuration);
+        $decodedJsonResponseBody = $this->retrieveItems($itemsRetrievalLimit);
+
         $itemsCount = $this->countItemsInResponse($decodedJsonResponseBody);
 
         $this->displayCountOfRetrievedItems($itemsCount);
+    }
+
+    /**
+     * @param InputInterface $input
+     * @return bool
+     */
+    protected function isInteractiveMode(InputInterface $input)
+    {
+        return !$input->hasOption('no-interaction') || !$input->getOption('no-interaction');
     }
 
     /**
@@ -97,15 +151,6 @@ class RetrievePocketDataCommand extends Command
     }
 
     /**
-     * @param $configurationFile
-     * @return bool
-     */
-    protected function canNotFindConfigurationFile($configurationFile)
-    {
-        return !file_exists($configurationFile);
-    }
-
-    /**
      * @param $configuration
      * @return bool
      */
@@ -117,23 +162,22 @@ class RetrievePocketDataCommand extends Command
     }
 
     /**
-     * @param $configurationFile
+     * @param $filePath
      * @return array|bool|mixed|null
      */
-    protected function parseConfiguration($configurationFile)
+    protected function parseConfiguration($filePath)
     {
         $parser = new Parser();
 
-        return $parser->parse(file_get_contents($configurationFile));
+        return $parser->parse(file_get_contents($filePath));
     }
 
     /**
-     * @param $configuration
      * @return string
      */
-    protected function buildItemsRetrievalEndpoint($configuration)
+    protected function buildItemsRetrievalEndpoint()
     {
-        return $configuration['pocket']['endpoint'] . '/get';
+        return $this->configuration['pocket']['endpoint'] . '/get';
     }
 
     /**
@@ -177,28 +221,42 @@ class RetrievePocketDataCommand extends Command
     }
 
     /**
-     * @param $configuration
+     * Retrieves items by sending an HTTP request to configured endpoint.
+     *
+     * @param $limit
      * @return mixed
      */
-    protected function retrieveItems($configuration)
+    protected function retrieveItems($limit)
     {
-        /** @var \GuzzleHttp\Message\ResponseInterface $response */
-        $response = $this->client->post(
-            $this->buildItemsRetrievalEndpoint($configuration),
-            [
-                'headers' => $this->getHeaders(),
-                'body' => [
-                    'access_token' => $configuration['pocket']['access_token'],
-                    'consumer_key' => $configuration['pocket']['consumer_key'],
-                    'count' => self::ITEMS_RETRIEVAL_LIMIT,
-                    'detailType' => 'complete'
+        try {
+            /** @var \GuzzleHttp\Message\ResponseInterface $response */
+            $response = $this->httpClient->post(
+                $this->buildItemsRetrievalEndpoint(),
+                [
+                    'headers' => $this->getHeaders(),
+                    'body' => [
+                        'access_token' => $this->configuration['pocket']['access_token'],
+                        'consumer_key' => $this->configuration['pocket']['consumer_key'],
+                        'count' => $limit,
+                        'detailType' => 'complete',
+                        'since' => 1283810400,
+                        'offset' => 0,
+                    ]
                 ]
-            ]
-        );
-        $decodedJsonResponseBody = $response->json();
-        $this->displayResponseError($decodedJsonResponseBody);
+            );
+            $decodedJsonResponseBody = $response->json();
 
-        return $decodedJsonResponseBody;
+            $targetDocument = $this->targetDirectory . '/' . date('Ymd_H:i:s') . '_document.json';
+            file_put_contents($targetDocument, json_encode($decodedJsonResponseBody, JSON_PRETTY_PRINT));
+
+            $this->displayResponseError($decodedJsonResponseBody);
+
+            return $decodedJsonResponseBody;
+        } catch (ServerException $serverException) {
+            $this->output->writeln($serverException->getMessage());
+
+            return [];
+        }
     }
 
     /**
@@ -209,5 +267,70 @@ class RetrievePocketDataCommand extends Command
         $this->translator = new Translator('en_GB', new MessageSelector());
         $this->translator->addLoader('yaml', new YamlFileLoader());
         $this->translator->addResource('yaml', __DIR__ . '/../../Resources/translations/command.en.yml', 'en_GB', 'command');
+    }
+
+    /**
+     * @return array|bool|mixed|null
+     */
+    protected function validateConfigurationReadability()
+    {
+        if (!$this->isConfigurationFileReadable($this->configurationFilePath)) {
+            ConfigurationException::throwNotFoundConfigurationException($this->configurationFilePath);
+        }
+    }
+
+    /**
+     * Returns true when the configuration file exists and it is readable, false otherwise.
+     *
+     * @param $file
+     * @return bool
+     */
+    protected function isConfigurationFileReadable($file)
+    {
+        return file_exists($file) || is_readable($file);
+    }
+
+    protected function validateConsumerKey()
+    {
+        if ($this->isConsumerKeyMissing($this->configuration)) {
+            ConfigurationException::throwMissingConsumerKey($this->configurationFilePath);
+        }
+    }
+
+
+    protected function validateTargetDirectory()
+    {
+        if (!is_dir($this->targetDirectory)) {
+            ConfigurationException::throwInvalidTargetDirectory($this->targetDirectory, $this->configurationFilePath);
+            throw new ConfigurationException('The configured target directory is not a valid directory.');
+        }
+    }
+
+    /**
+     * Asks user about the number of items to be retrieved or pick the default value.
+     *
+     * @return int|null|string
+     */
+    protected function getItemsRetrievalLimit()
+    {
+        if ($this->isInteractiveMode($this->input)) {
+            $askForItemRetrievalLimitMessage = $this->translator->trans(
+                'dialog.ask.items_retrieval_limit', [], 'command', 'en_GB') . "\n";
+            $question = new Question($askForItemRetrievalLimitMessage);
+            $question->setValidator(function ($answer) {
+                if (!is_numeric($answer) or $answer < 0) {
+                    throw new \RuntimeException('The number of items to be retrieved should be a positive integer');
+                }
+
+                return $answer;
+            });
+            /** @var \Symfony\Component\Console\Helper\QuestionHelper $dialog */
+            $dialog = $this->getHelper('question');
+            $itemsRetrievalLimit = $dialog->ask($this->input, $this->output, $question);
+        } else {
+            $itemsRetrievalLimit = self::DEFAULT_ITEMS_RETRIEVAL_LIMIT;
+        }
+
+        return $itemsRetrievalLimit;
     }
 }
--- Tests/Console/Command/RetrievePocketDataCommandTest.php
+++ Tests/Console/Command/RetrievePocketDataCommandTest.php
@@ -8,9 +8,13 @@ use Prophecy\Prophet;
 use Symfony\Component\Console\Application;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Tester\CommandTester;
+use Symfony\Component\Console\Tests\Helper\HelperSetTest;
 
+use Symfony\Component\Filesystem\Filesystem;
+
+use Symfony\Component\Finder\Finder;
 use WeavingTheWeb\OwnYourData\Console\Command\RetrievePocketDataCommand;
-use WeavingTheWeb\OwnYourData\Exception\InstallationException;
+use WeavingTheWeb\OwnYourData\Exception\ConfigurationException;
 
 /**
  * @author  Thierry Marianne <thierry.marianne@weaving-the-web.org>
@@ -20,9 +24,9 @@ class RetrievePocketDataCommandTest extends \PHPUnit_Framework_TestCase
     const COMMAND_NAME = 'weaving_the_web:pocket:retrieve';
 
     /**
-     * @var RetrievePocketDataCommand
+     * @var Application
      */
-    private $testedCommand;
+    private $application;
 
     /**
      * @var CommandTester
@@ -30,14 +34,34 @@ class RetrievePocketDataCommandTest extends \PHPUnit_Framework_TestCase
     private $commandTester;
 
     /**
-     * @var Application
+     * @var integer
      */
-    private $application;
+    private $defaultItemsRetrievalLimit;
+
+    /**
+     * @var RetrievePocketDataCommand
+     */
+    private $testedCommand;
+
+    /**
+     * @var \Symfony\Component\Filesystem\Filesystem
+     */
+    private $filesystem;
+
+    /**
+     * @var \Symfony\Component\Finder\Finder
+     */
+    private $finder;
 
     /**
      * @var Prophet
      */
     private $prophet;
+
+    /**
+     * @var string
+     */
+    private $targetDirectory;
 
     public function setUp()
     {
@@ -56,15 +80,36 @@ class RetrievePocketDataCommandTest extends \PHPUnit_Framework_TestCase
 
         $this->application->add($retrievePocketDataCommand);
 
-        $this->testedCommand = $this->application->find(self::COMMAND_NAME);
-        $this->testedCommand->client = $this->mockHttpClient();
+        $testedCommand = $this->application->find(self::COMMAND_NAME);
+
+        $this->defaultItemsRetrievalLimit = 2;
+
+        $testedCommand->httpClient = $this->mockHttpClient();
+
+        $this->testedCommand = $testedCommand;
 
         $this->commandTester = new CommandTester($this->testedCommand);
+
+        $this->filesystem = new Filesystem();
+
+        $this->finder = new Finder();
+
+        $this->targetDirectory = __DIR__ . '/../../../Resources/documents';
     }
 
     public function tearDown()
     {
         $this->prophet->checkPredictions();
+
+        $this->finder
+            ->files()
+            ->depth(1)
+            ->in($this->targetDirectory)
+            ->name('*document.json');
+
+        foreach ($this->finder as $document) {
+            $this->filesystem->remove($document);
+        }
 
         parent::tearDown();
     }
@@ -72,67 +117,98 @@ class RetrievePocketDataCommandTest extends \PHPUnit_Framework_TestCase
     /**
      * @test
      */
-    public function it_should_throw_an_installation_exception_when_executed_without_configuration()
+    public function it_should_display_an_error_message_when_executed_without_configuration()
     {
-        $this->assertThrowNotFoundConfigurationException(self::COMMAND_NAME);
+        $retrievePocketDataCommand = $this->application->find(self::COMMAND_NAME);
+
+        $retrievePocketDataCommand->configurationFilename = 'non_existing_configuration_file.yml';
+        $returnCode = $this->commandTester->execute($this->getNoInteractionInput());
+
+        $this->assertEquals($returnCode, ConfigurationException::CONFIGURATION_NOT_FOUND,
+            'It should return the "not found configuration" error code.');
     }
 
     /**
      * @test
      */
-    public function it_should_throw_an_installation_exception_when_executed_without_consumer_key()
+    public function it_should_throw_an_configuration_exception_when_executed_without_consumer_key()
     {
-        $this->assertThrowMissingConsumerKey(self::COMMAND_NAME);
+        $retrievePocketDataCommand = $this->application->find(self::COMMAND_NAME);
+
+        $retrievePocketDataCommand->configurationFilename = 'config.yml.dist';
+        $returnCode = $this->commandTester->execute($this->getNoInteractionInput());
+
+        $this->assertEquals($returnCode, ConfigurationException::CONSUMER_KEY_MISSING,
+            'It should return the "missing consumer key" error code.');
+    }
+    
+    /**
+     * @test
+     */
+    public function it_should_throw_an_configuration_exception_when_executed_with_invalid_target_directory()
+    {
+        $retrievePocketDataCommand = $this->application->find(self::COMMAND_NAME);
+
+        $testConfigurationDirectory = $this->getRelativeTestConfigurationDirectory();
+        $retrievePocketDataCommand->configurationFilename = $testConfigurationDirectory .
+            '/config_with_invalid_target_directory.yml';
+        $returnCode = $this->commandTester->execute($this->getNoInteractionInput());
+
+        $this->assertEquals($returnCode, ConfigurationException::TARGET_DIRECTORY_INVALID,
+            'It should return the "invalid target directory" error code.');
+    }
+
+    /**
+     * @return string
+     */
+    public function getRelativeTestConfigurationDirectory()
+    {
+        return '/../../Tests/Resources/config';
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_ask_the_user_for_the_items_retrieval_limit()
+    {
+        $this->setUpValidConfiguration();
+
+        $dialog = $this->testedCommand->getHelper('question');
+        $dialog->setInputStream($this->getInputStream(sprintf("%d\n", $this->defaultItemsRetrievalLimit)));
+
+        $this->commandTester->execute(['command' => self::COMMAND_NAME]);
+        $this->assertCountItemsDisplayed($this->defaultItemsRetrievalLimit);
+    }
+
+    public function getInputStream($input)
+    {
+        $stream = fopen('php://memory', 'r+', false);
+        fputs($stream, $input);
+        rewind($stream);
+
+        return $stream;
     }
 
     public function testExecute()
     {
-        $this->testedCommand->configurationFilename = '../../Tests/Resources/config/config.yml';
-        $this->commandTester->execute(['command' => self::COMMAND_NAME]);
+        $this->setUpValidConfiguration();
+
+        $this->commandTester->execute($this->getNoInteractionInput());
 
         $display = $this->commandTester->getDisplay();
         $unexpectedExceptionMessage = 'Exception';
         $this->assertNotContains($unexpectedExceptionMessage, $display,
             sprintf('Executing the command should not display an exception message.', $unexpectedExceptionMessage));
 
-        $expectedMessage = '2 items have been retrieved';
-        $this->assertContains($expectedMessage, $display,
-            'Command execution should output the number of items retrieved');
+        $this->assertCountItemsDisplayed($this->defaultItemsRetrievalLimit);
     }
 
     /**
-     * @param $commandName
-     * @return \Exception
+     * @return array
      */
-    protected function assertThrowNotFoundConfigurationException($commandName)
+    protected function getNoInteractionInput()
     {
-        $retrievePocketDataCommand = $this->application->find($commandName);
-
-        try {
-            $retrievePocketDataCommand->configurationFilename = 'non_existing_configuration_file.yml';
-            $this->commandTester->execute(['command' => $commandName]);
-        } catch (\Exception $exception) {
-            $this->assertInstanceOf('WeavingTheWeb\OwnYourData\Exception\InstallationException', $exception);
-            $this->assertEquals(InstallationException::CONFIGURATION_NOT_FOUND, $exception->getCode());
-
-            return $exception;
-        }
-    }
-
-    /**
-     * @param $commandName
-     */
-    protected function assertThrowMissingConsumerKey($commandName)
-    {
-        $retrievePocketDataCommand = $this->application->find($commandName);
-
-        try {
-            $retrievePocketDataCommand->configurationFilename = 'config.yml.dist';
-            $this->commandTester->execute(['command' => $commandName]);
-        } catch (\Exception $exception) {
-            $this->assertInstanceOf('WeavingTheWeb\OwnYourData\Exception\InstallationException', $exception);
-            $this->assertEquals(InstallationException::CONSUMER_KEY_MISSING, $exception->getCode());
-        }
+        return ['command' => self::COMMAND_NAME, '--no-interaction' => true];
     }
 
     protected function mockHttpClient()
@@ -146,5 +222,25 @@ class RetrievePocketDataCommandTest extends \PHPUnit_Framework_TestCase
         $httpClientProphecy->post(Argument::cetera())->willReturn($responseProphecy->reveal());
 
         return $httpClientProphecy->reveal();
+    }
+
+    protected function setUpValidConfiguration()
+    {
+        $testConfigurationDirectory = $this->getRelativeTestConfigurationDirectory();
+
+        $this->testedCommand->configurationFilename = $testConfigurationDirectory . '/config.yml';
+    }
+
+    /**
+     * @param $expectedItemsCount
+     */
+    protected function assertCountItemsDisplayed($expectedItemsCount)
+    {
+        $expectedMessage = $expectedItemsCount . ' items have been retrieved';
+        $this->assertContains(
+            $expectedMessage,
+            $this->commandTester->getDisplay(),
+            'Command execution should output the number of items retrieved'
+        );
     }
 }
